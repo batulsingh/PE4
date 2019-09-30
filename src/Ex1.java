24db8fb15a6e42b2d6bf6ee81d8fc83d18fccf77
bhaiya.singh <bhaiya.singh@cgi.com>
2019-09-30 10:03:52 +0530

    Second Commit

---
+++ src/Ex1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string :");
+        String strInput = sc.nextLine();
+
+        System.out.print("Enter the character to count number of occurrences :");
+        char ch = sc.next().charAt(0);
+
+        int lengthOfStr = strInput.length();
+        String str2 = strInput.replaceAll("[ch]","");
+
+        int lengthOfStringWithoutCh = str2.length();
+
+        int numOfOccurrences;
+
+        numOfOccurrences = lengthOfStr - lengthOfStringWithoutCh;
+
+        System.out.println("The character "+ch+" occurred "+numOfOccurrences+" times in the string");
+        System.out.print(str2);
+    }
+}
---
+++ src/demo.java
@@ -0,0 +1,13 @@
+
+public  class demo{
+
+
+
+    public int multiply(int a, int b){
+        return (a*b);
+    }
+
+    public int add(int a , int b){
+        return (a+b);
+    }
+}
